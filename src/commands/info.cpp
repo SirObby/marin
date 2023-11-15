6bd6fcebf631cf3c359f6a2bc5997982c4550ab2
SirObby <70753342+SirObby@users.noreply.github.com>
2023-11-15 21:53:21 +0200

    change the invite link

--- src/commands/info.cpp
+++ src/commands/info.cpp
@@ -13,7 +13,7 @@ namespace Commands
                 ember emb;
                 emb.add_description("I am Marin Kitagawa, a discord bot for receiving anime recommendations based on your anime tastes.\n\n/get-started to get started!");
                 emb.set_title("Info! ~ ♡");
-                emb.add_field("invite the bot", "[here](https://discord.com/api/oauth2/authorize?client_id=1132282262541582347&permissions=17180183616&scope=bot)", true);
+                emb.add_field("invite the bot", "[here](https://discord.com/api/oauth2/authorize?client_id=1132282262541582347&permissions=313408&scope=bot%20applications.commands)", true);
                 emb.add_field("support server", "[here](https://discord.gg/4kH9gWErcP)", true);
                 m.set_content("");
                 m.add_embed(emb.return_emb());
