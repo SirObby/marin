6f267334fcd9cde3d8e30fe612462f0c4800dc86
Samuel Hautamäki <samuelhautamaki@noreply.codeberg.org>
2026-05-10 14:12:39 +0300

    better info command

--- src/commands/info.cpp
+++ src/commands/info.cpp
@@ -5,9 +5,12 @@ namespace commands {
     void info(dpp::slashcommand_t event) {
         dpp::embed emb = dpp::embed()
             .set_color(0xe7c4ca)
-            .set_title("Marin Bot")
-            .set_description("A Discord bot for interfacing with the AniList API.")
-            .add_field("Library", std::format("D++ {}", dpp::utility::version()), true);
+            .set_title("Info! ~ ♡")
+            .set_description("I am Marin Kitagawa, a discord bot for searching for information about an anime based on the title.\n\n/search to get started!")
+            .add_field("invite the bot", "[here](https://discord.com/api/oauth2/authorize?client_id=1132282262541582347&permissions=313408&scope=bot%20applications.commands)", true)
+            .add_field("support server", "[here](https://discord.gg/4kH9gWErcP)", true)
+            .add_field("Library", std::format("D++ {}", dpp::utility::version()), true)
+            .add_field("Servers", std::to_string(dpp::get_guild_cache()->count()), true);
         event.reply(dpp::message().add_embed(emb));
     }
 }
